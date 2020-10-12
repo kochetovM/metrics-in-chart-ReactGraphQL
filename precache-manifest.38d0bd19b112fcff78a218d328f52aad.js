fa4aeb48511dd88107c95b684b7e373ce64321c3
Misha K <easydriverup@gmail.com>
2020-10-12 02:35:47 -0500

    Updates

---
+++ precache-manifest.38d0bd19b112fcff78a218d328f52aad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd938c9828ed2a64508038ebdf39f828",
+    "url": "/metrics-in-chart-ReactGraphQL/index.html"
+  },
+  {
+    "revision": "001e8dac9732c9e2a398",
+    "url": "/metrics-in-chart-ReactGraphQL/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "001e8dac9732c9e2a398",
+    "url": "/metrics-in-chart-ReactGraphQL/static/js/2.7449dc93.chunk.js"
+  },
+  {
+    "revision": "222583c76dc626a73805a997ec5030b4",
+    "url": "/metrics-in-chart-ReactGraphQL/static/js/2.7449dc93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad248300b16ad10581b8",
+    "url": "/metrics-in-chart-ReactGraphQL/static/js/main.aa1ea799.chunk.js"
+  },
+  {
+    "revision": "53b445ea25031247c3c6",
+    "url": "/metrics-in-chart-ReactGraphQL/static/js/runtime-main.4216e6cc.js"
+  }
+]);
